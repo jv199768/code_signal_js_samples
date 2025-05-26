f89b866920dfc213a95ffce35128f63f22d2c104
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 19:57:33 -0400

    Create myScore3.js

---
+++ myScore3.js
@@ -0,0 +1,12 @@
+let myScore = 80;
+let friendScore = 80;
+// If our score is higher, we brag.
+// If our score is lower, we console ourselves.
+// If our scores are tied, we declare a stalemate.
+if (myScore > friendScore) {
+    console.log("I scored higher!");
+} else if (myScore < friendScore) {
+    console.log("Oh no, my friend scored higher!")
+} else {
+    console.log("It's a tie game!")
+}
