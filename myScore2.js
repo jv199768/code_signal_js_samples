81326f04da3f1a34570cae6bdbb9f66bb01be057
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 19:56:49 -0400

    Create myScore2.js

---
+++ myScore2.js
@@ -0,0 +1,12 @@
+let myScore = 60;
+let friendScore = 80;
+// If our score is higher, we brag. Else, we console ourselves!
+if (myScore > friendScore) {
+    console.log("I scored higher!");
+    console.log("Hurrah!");
+} else {
+    console.log("My friend scored higher. Well played.");
+    console.log("I’ll come back stronger next time.");
+}
+
+console.log("Game over.");
