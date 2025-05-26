4ef73450d22560787a84717516c5913ecf7b44b4
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 19:56:28 -0400

    Create myScore1.js

---
+++ myScore1.js
@@ -0,0 +1,9 @@
+let myScore = 100;
+let friendScore = 80;
+// If our score is higher, we get to brag about it!
+if (myScore > friendScore) {
+    console.log("Score bragging rights are mine!");
+    console.log("Victory is sweet!");
+}
+
+console.log("Game over.");
