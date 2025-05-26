04c4ff82efacd57d9528e08d617488e7b55f65a6
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 19:50:17 -0400

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,25 @@
+let age = 20;
+const year = 1900; // A constant. Its value can't change.
+
+age = 21; // Variables created with let can be reassigned.
+let flowerJar = null;         // we have an empty flower jar
+console.log(flowerJar);       // outputs: null
+
+let candyJar;                 // we have a label for a candy jar, but no actual jar
+console.log(candyJar);        // outputs: undefined
+let myFavoriteColor;
+let message1;
+let $location;
+
+let x = 10; // x is 10
+x += 5; // adding 5 to x makes x 15
+x -= 3; // subtracting 3 from x makes x 12
+x *= 2; // multiplying x by 2 makes x 24
+
+let x = 5; // x is now 5
+
+console.log(5 == '5'); // true
+console.log(5 === '5'); // false
+
+console.log(5 != '5'); // false
+console.log(5 !== '5'); // true
