791b2c3bd108b55a24954a0754acb4ab5a89a960
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 19:41:08 -0400

    Create hello_world_2.js

---
+++ hello_world_2.js
@@ -0,0 +1,5 @@
+console.log(2 + 2); // Outputs: 4
+console.log("2 + 2"); // Outputs: 2 + 2
+console.log("Hello, " + "World!"); // Outputs: Hello, World!
+console.log("2" + "2"); // Outputs: 22, because + operation applied on strings concatenates them
+console.log(2 + 2 > 5); // Outputs: false, because 2 + 2 is not greater than 5.
