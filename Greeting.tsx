be9f68542c51b3ce3b5d780449eae83302309c02
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 21:44:27 -0400

    Create Greeting.tsx

---
+++ Greeting.tsx
@@ -0,0 +1,8 @@
+import React from 'react'; // importing React to use React.Component;
+
+// Defining Greeting as a class component
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hello, JavaScript!</h1>; 
+  }
+}
