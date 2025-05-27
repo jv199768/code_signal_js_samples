d3f3eb8a7a5a7f4e7298cd5616b16b07546a946c
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 20:27:37 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+let greet = 'Hello';
+let name = 'John';
+// Here, we're embedding `greet` and `name` variables within a template string
+let message = `${greet}, ${name}! How are you today?`;
+console.log(message); // prints "Hello, John! How are you today?"
+function greet() {
+    return "Hello, World!";
+}
+function addFive(num) {
+    return num + 5;
+}
+function multiply(a, b) {
+    return a * b;
+}
