c30e06e88f841a15910cbcc71b2291b7618a257e
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 20:08:04 -0400

    Create square.js

---
+++ square.js
@@ -0,0 +1,7 @@
+// function that returns the square of a number
+function square(number) {
+  return number * number; // number is the function parameter
+}
+
+const squaredNumber = square(5);
+console.log(squaredNumber);  // Outputs: 25
