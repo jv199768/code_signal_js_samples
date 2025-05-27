753b2c462c75e49e3c4163efef36bac35b875e14
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 22:26:36 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = {
+    posts: [
+        { // First blog post
+            id: 1,
+            title: 'Hello, world!',
+            content: 'This is my first blog post.',
+        },
+        { // Second blog post
+            id: 2,
+            title: 'Goodbye, world!',
+            content: 'This is my last blog post.',
+        },
+    ]
+};
