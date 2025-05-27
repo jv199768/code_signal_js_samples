10ec8f9d478a9ccac21095f421d8241fe18b0d9f
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 20:12:06 -0400

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,13 @@
+let rainbowColors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+console.log(rainbowColors[0]); // Outputs "Red"
+let variedArray = ["Hello", 25, true]; // The `variedArray` contains three different types of data.
+let fruits = ["Apple", "Banana", "Mango"];
+fruits[1] = "Grape";  // "Banana" is now "Grape"
+console.log(fruits[1]); // Outputs "Grape"
+let colors = ["Red", "Green", "Blue"];
+console.log(colors.length); // Outputs 3
+let colors = ["Red", "Green", "Blue"];
+colors.push("Yellow");  // Yellow added
+console.log(colors);  // Output: ["Red", "Green", "Blue", "Yellow"]
+colors.pop();  // Yellow removed
+console.log(colors);  // Output: ["Red", "Green", "Blue"]
